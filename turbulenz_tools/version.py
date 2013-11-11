8d7431d1b682580a83e14e9a3344461b0223b070
Duncan Tebbs <duncantebbs@turbulenz.com>
2013-11-11 16:31:23 +0900

    Tick version to 0.28.0

--- turbulenz_tools/version.py
+++ turbulenz_tools/version.py
@@ -3,4 +3,4 @@
 # Must be kept in sync with the version in the plugin and webgl engine
 # code.  Python tools use this to stamp build products and to generate
 # version checking code.
-SDK_VERSION = "0.27.0.0"
+SDK_VERSION = "0.28.0.0"
